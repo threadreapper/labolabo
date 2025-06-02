db4eac340ca91a295f497259cbbaa59b1cedaf7b
Pavel Zhukov <zhukowgg@gmail.com>
2025-06-02 18:34:26 +0600

    fix tests

--- RefCounter.c
+++ RefCounter.c
@@ -1,35 +1,34 @@
 #include "RefCounter.h"
-
 #include <stdlib.h>
 
 int initialize_ref_allocator(PoolAllocator *allocator, size_t block_size,
                              size_t num_blocks) {
-  return initialize_pool(allocator, block_size, num_blocks);
+    return initialize_pool(allocator, block_size, num_blocks);
 }
 
 ref_count_t *create_ref_object(PoolAllocator *allocator, size_t object_size) {
-  ref_count_t *ref = (ref_count_t *)allocate_from_pool(allocator);
-  if (!ref) return NULL;
+    ref_count_t *ref = (ref_count_t *)allocate_from_pool(allocator);
+    if (!ref) return NULL;
 
-  ref->object = malloc(object_size);
-  if (!ref->object) {
-    free_to_pool(allocator, ref);
-    return NULL;
-  }
+    ref->object = malloc(object_size);
+    if (!ref->object) {
+        free_to_pool(allocator, ref);
+        return NULL;
+    }
 
-  ref->count = 1;
-  return ref;
+    ref->count = 1; 
+    return ref;
 }
 
 void retain_ref(ref_count_t *ref) {
-  if (ref) ref->count++;
+    if (ref) ref->count++;
 }
 
 void release_ref(PoolAllocator *allocator, ref_count_t *ref) {
-  if (!ref || !allocator) return;
+    if (!ref || !allocator) return;
 
-  if (--ref->count == 0) {
-    free(ref->object);
-    free_to_pool(allocator, ref);
-  }
+    if (--ref->count == 0) {
+        free(ref->object);       
+        free_to_pool(allocator, ref); 
+    }
 }
