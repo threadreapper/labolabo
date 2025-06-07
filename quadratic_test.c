504c491e15c69660ee9748ba3e72be5547ee317e
Pavel Zhukov <zhukowgg@gmail.com>
2025-06-07 17:03:50 +0600

    fix

--- quadratic_test.c
+++ quadratic_test.c
@@ -3,106 +3,115 @@
 #include <math.h>
 #include <stdio.h>
 
-void root_test_1() {
-  double a = 0;
-  double b = 1;
-  double c = 1;
-  double e = 0;
-  double roots[2];
-  int root_count;
+void test_zero_a() {
+    double a = 0;
+    double b = 1;
+    double c = 1;
+    double epsilon = 1e-9;
+    double roots[2];
+    int root_count;
 
-  find_roots(a, b, c, roots, &root_count, e);
+    find_roots(a, b, c, roots, &root_count, epsilon);
 
-  assert(0 == root_count);
+    assert(0 == root_count);
 }
 
-void root_test_2() {
-  double a = 1;
-  double b = 0;
-  double c = -1;
-  double e = 0;
-  double roots[2];
-  int root_count;
-  find_roots(a, b, c, roots, &root_count, 0);
+void test_two_real_roots() {
+    double a = 1;
+    double b = 0;
+    double c = -1;
+    double epsilon = 1e-9;
+    double roots[2];
+    int root_count;
 
-  assert(2 == root_count);
-  assert(-1 == roots[0]);
-  assert(1 == roots[1]);
+    find_roots(a, b, c, roots, &root_count, epsilon);
+
+    assert(2 == root_count);
+    assert(fabs(-1 - roots[0]) < epsilon);
+    assert(fabs(1 - roots[1]) < epsilon);
 }
-void root_test_3() {
-  double a = 1;
-  double b = 0;
-  double c = 0;
-  double e = 0;
-  double roots[2];
-  int root_count;
 
-  find_roots(a, b, c, roots, &root_count, e);
+void test_one_real_root() {
+    double a = 1;
+    double b = 0;
+    double c = 0;
+    double epsilon = 1e-9;
+    double roots[2];
+    int root_count;
 
-  assert(1 == root_count);
-  assert(roots[0] == 0);
+    find_roots(a, b, c, roots, &root_count, epsilon);
+
+    assert(1 == root_count);
+    assert(fabs(0 - roots[0]) < epsilon);
 }
-void root_test_4() {
-  double a = 1;
-  double b = 0;
-  double c = 1;
-  double e = 0;
-  double roots[2];
-  int root_count;
 
-  find_roots(a, b, c, roots, &root_count, e);
+void test_no_real_roots() {
+    double a = 1;
+    double b = 0;
+    double c = 1;
+    double epsilon = 1e-9;
+    double roots[2];
+    int root_count;
 
-  assert(0 == root_count);
+    find_roots(a, b, c, roots, &root_count, epsilon);
+
+    assert(0 == root_count);
 }
-void root_test_5() {
-  double a = 1;
-  double b = 0;
-  double c = -1E-7;
-  double roots[2];
-  int root_count;
-  double epsilon = 1e-8;
 
-  find_roots(a, b, c, roots, &root_count, epsilon);
+void test_small_c_positive_discriminant() {
+    double a = 1;
+    double b = 0;
+    double c = -1E-7;
+    double epsilon = 1e-9;
+    double roots[2];
+    int root_count;
 
-  assert(2 == root_count);
-  assert(fabs(-3E-4 - roots[0]) < 0.0001);
-  assert(fabs(3E-4 - roots[1]) < 0.0001);
+    find_roots(a, b, c, roots, &root_count, epsilon);
+
+    assert(2 == root_count);
+    assert(fabs(-3E-4 - roots[0]) < 1e-4);
+    assert(fabs(3E-4 - roots[1]) < 1e-4);
 }
-void root_test_6() {
-  double a = 1;
-  double b = -1E+10;
-  double c = -1;
-  double roots[2];
-  int root_count;
-  double epsilon = 1e-11;
 
-  find_roots(a, b, c, roots, &root_count, epsilon);
+void test_large_b_positive_discriminant() {
+    double a = 1;
+    double b = -1E+10;
+    double c = -1;
+    double epsilon = 1e-11;
+    double roots[2];
+    int root_count;
 
-  assert(2 == root_count);
+    find_roots(a, b, c, roots, &root_count, epsilon);
 
-  assert(fabs(-1E-10 - roots[0]) < epsilon);
-  assert(fabs(1E+10 - roots[1]) < epsilon);
+    assert(2 == root_count);
+    assert(fabs(-1E-10 - roots[0]) < epsilon);
+    assert(fabs(1E+10 - roots[1]) < epsilon);
 }
-void root_test_7() {
-  double a = 1;
-  double b = 0;
-  double c = -1e-8;
-  double epsilon = 1e-7;
-  double roots[2];
-  int root_count;
 
-  find_roots(a, b, c, roots, &root_count, epsilon);
-  assert(1 == root_count);
-  assert(fabs(0 - roots[0]) < epsilon);
+void test_very_small_c_positive_discriminant() {
+    double a = 1;
+    double b = 0;
+    double c = -1e-8;
+    double epsilon = 1e-9;
+    double roots[2];
+    int root_count;
+
+    find_roots(a, b, c, roots, &root_count, epsilon);
+
+    assert(2 == root_count);
+    assert(fabs(-sqrt(1e-8) - roots[0]) < epsilon);
+    assert(fabs(sqrt(1e-8) - roots[1]) < epsilon);
 }
+
 int main() {
-  root_test_1();
-  root_test_2();
-  root_test_3();
-  root_test_4();
-  root_test_5();
-  root_test_6();
-  root_test_7();
+    test_zero_a();
+    test_two_real_roots();
+    test_one_real_root();
+    test_no_real_roots();
+    test_small_c_positive_discriminant();
+    test_large_b_positive_discriminant();
+    test_very_small_c_positive_discriminant();
 
-  return 0;
+    printf("All tests passed!\n");
+    return 0;
 }
